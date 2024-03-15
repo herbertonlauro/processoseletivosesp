07530b2f9e6f0269e9dc44218981c15c6f7b9ab3
Herberton <herberton.dev@gmail.com>
2024-03-15 12:33:54 -0400

    Criado pasta model com as entidades Endereco e Pessoa

---
+++ src/main/java/com/seletivo/sesp/model/Endereco.java
@@ -0,0 +1,53 @@
+package com.seletivo.sesp.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "endereco")
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "logradouro")
+    @NotBlank(message = "não pode ser nulo ou vazio")
+    private String logradouro;
+
+    @Column(name = "bairro")
+    @NotBlank(message = "não pode ser nulo ou vazio")
+    private String bairro;
+
+    @Column(name = "numero")
+    @NotNull(message = "não pode ser nulo")
+    private int numero;
+
+    @Column(name = "cidade")
+    @NotBlank(message = "não pode ser nulo ou vazio")
+    private String cidade;
+
+    @Column(name = "estado")
+    @NotBlank(message = "não pode ser nulo ou vazio")
+    private String estado;
+
+    @Column(name = "cep")
+    @NotBlank(message = "não pode ser nulo ou vazio")
+    private String cep;
+
+    @JsonBackReference
+    @OneToOne
+    @JoinColumn(name = "idPessoa")
+    private Pessoa pessoa;
+
+}
---
+++ src/main/java/com/seletivo/sesp/model/Pessoa.java
@@ -0,0 +1,77 @@
+package com.seletivo.sesp.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "pessoa")
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "não pode ser nulo ou vazio")
+    @Schema(example = "Gustavo Lemos", required = true)
+    @Column(name = "m")
+    private String nome;
+
+    @NotBlank(message = "não pode ser nulo ou vazio")
+    @Schema(example = "123456", required = true)
+    private String rg;
+
+    @CPF
+    @NotBlank(message = "não pode ser nulo ou vazio")
+    @Schema(example = "90547837887", required = true)
+    @Column(unique = true)
+    private String cpf;
+
+    @NotNull(message = "não pode ser nulo")
+    @Schema(example = "2022-03-10", required = true)
+    private LocalDate nascimento;
+
+    @Email(message = "email invalido")
+    @NotBlank(message = "não pode ser nulo ou vazio")
+    @Schema(example = "seuemail@gmail.com", required = true)
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank(message = "não pode ser nulo ou vazio")
+    @Schema(example = "(83)99565-2978", required = true)
+    private String telefone;
+
+    @NotBlank(message = "não pode ser nulo ou vazio")
+    @Schema(example = "Elisa Isa", required = true)
+    private String mae;
+
+    @NotBlank(message = "não pode ser nulo ou vazio")
+    @Schema(example = "Roberto isa", required = true)
+    private String pai;
+
+    @NotNull(message = "não pode ser nulo")
+    @Schema(example = "2022-03-10", required = true)
+    private LocalDate cadastradoEm;
+
+    @JsonManagedReference
+    @OneToOne(mappedBy = "pessoa")
+    private Endereco endereco;
+
+    @PrePersist
+    public void prePersist() {
+        cadastradoEm = LocalDate.now();
+    }
+}
