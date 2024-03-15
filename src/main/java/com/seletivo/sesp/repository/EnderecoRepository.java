5892f35940df8a31f22fd87ae9bd0a82a7a14706
Herberton <herberton.dev@gmail.com>
2024-03-15 13:14:17 -0400

    Implementado o repository

---
+++ src/main/java/com/seletivo/sesp/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.seletivo.sesp.repository;
+
+import com.seletivo.sesp.model.Endereco;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class EnderecoRepository implements PanacheRepository<Endereco> {
+}
